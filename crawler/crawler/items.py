c143bcd4e94688164a39fdec2a30eb235589fd7d
Vadim Saprykin <sprkweb@yandex.ru>
2022-02-02 12:19:50 +0300

    content extraction

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -1,12 +1,10 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
+from dataclasses import dataclass
 import scrapy
 
-
 class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    link = scrapy.Field()
+    last_updated = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
+    keywords = scrapy.Field()
+    text = scrapy.Field()
--- crawler/crawler/spiders/torspider.py
+++ crawler/crawler/spiders/torspider.py
@@ -1,11 +1,22 @@
+import time
 import scrapy
+
+from crawler.items import CrawlerItem
 
 class TorSpider(scrapy.Spider):
     name = 'torspider'
     start_urls = ['http://torcatalog.com']
 
     def parse(self, response):
+        item = CrawlerItem()
+        item['link'] = response.url
+        item['last_updated'] = time.time()
+        item['title'] = response.css('title::text').get()
+        item['description'] = response.css('meta[name=description]::attr(content)').get()
+        item['keywords'] = ', '.join(response.css('meta[name=keywords]::attr(content), h1::text, h2::text, h3::text, b::text, strong::text').getall())
+        item['text'] = ' '.join(response.css('body *::text').getall())
+        yield item
+
         for link in response.css('a::attr(href)').getall():
-            # yield response.follow(next_page, self.parse)
             if link.startswith('http') and '.onion' in link:
-                yield { 'link': link }
+                yield response.follow(link, self.parse)
