803736c18e1a16fb1c02be1c4a3fb8ec957de05f
Vadim Saprykin <sprkweb@yandex.ru>
2022-02-04 15:13:58 +0300

    add .svelte type to static.d.ts

--- frontend/types/static.d.ts
+++ frontend/types/static.d.ts
@@ -1,5 +1,6 @@
 /* Use this file to declare any custom file extensions for importing */
 /* Use this folder to also add/extend a package d.ts file, if needed. */
+/// <reference types="svelte" />
 
 /* CSS MODULES */
 declare module '*.module.css' {
